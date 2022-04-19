50f9292e4db7a2724715978f034a56ec28b216e9
spravcesystemu <89079372+spravcesystemu@users.noreply.github.com>
2022-04-19 17:09:58 +0300

    Update variables.tf

--- terraform/stage/variables.tf
+++ terraform/stage/variables.tf
@@ -6,14 +6,21 @@ variable folder_id {
 }
 variable zone {
   description = "Zone"
-  default = "ru-central1-a"
+  default     = "ru-central1-a"
 }
 variable region_id {
   description = "region"
   default     = "ru-central1"
 }
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
 variable image_id {
   description = "Disk image"
+}
+variable subnet_id {
+  description = "Subnet"
 }
 variable service_account_key_file {
   description = "key .json"
@@ -25,18 +32,15 @@ variable instances {
   description = "count instances"
   default     = 1
 }
+variable app_disk_image {
+  description = "disk image for reddit app"
+  default     = "reddit-app-base"
+}
 variable db_disk_image {
   description = "disk image for mongodb"
   default     = "reddit-db-base"
 }
-variable access_key {
-  description = "key id"
+variable prov {
+  description = "using provisioner"
+  default = true
 }
-variable secret_key {
-  description = "secret key"
-}
-variable bucket_name {
-  description = "bucket name"
-}
-
-
