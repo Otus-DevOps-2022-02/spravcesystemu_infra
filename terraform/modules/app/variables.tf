14c3ac373de36dde157c8ee8e57e425653f27fcb
spravcesystemu <89079372+spravcesystemu@users.noreply.github.com>
2022-04-20 16:07:14 +0300

    Update variables.tf

--- terraform/modules/app/variables.tf
+++ terraform/modules/app/variables.tf
@@ -1,10 +1,20 @@
-variable public_key_path {
+variable "public_key_path" {
   description = "Path to the public key used for ssh access"
 }
-variable app_disk_image {
+variable "app_disk_image" {
   description = "Disk image for reddit app"
-  default = "reddit-app-base"
+  default     = "reddit-app-base"
 }
-variable subnet_id {
+variable "subnet_id" {
   description = "Subnets for modules"
+}
+variable "private_key_path" {
+  description = "Path to the private key used for ssh access"
+}
+variable "deploy" {
+  description = "App deploying"
+  default     = true
+}
+variable db_ip {
+  description = "database IP"
 }
