c8bc9350bada5118392b352904cb4ff39406ca00
spravcesystemu <89079372+spravcesystemu@users.noreply.github.com>
2022-04-20 16:59:40 +0300

    Update main.tf

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -5,19 +5,22 @@ provider "yandex" {
   folder_id                = var.folder_id
   zone                     = var.zone
 }
-module "db" {
-  source           = "../modules/db"
-  public_key_path  = var.public_key_path
-  db_disk_image    = var.db_disk_image
-  subnet_id        = var.subnet_id
-  private_key_path = var.private_key_path
-}
 
 module "app" {
-  source           = "../modules/app"
-  public_key_path  = var.public_key_path
-  app_disk_image   = var.app_disk_image
-  subnet_id        = var.subnet_id
+  source          = "../modules/app"
+  public_key_path = var.public_key_path
   private_key_path = var.private_key_path
-  db_ip            = module.db.db_internal_ip
+  app_disk_image  = var.app_disk_image
+  subnet_id       = var.subnet_id
+  db_ip           = module.db.db_internal_ip
+  prov            = var.prov
+}
+
+module "db" {
+  source          = "../modules/db"
+  public_key_path = var.public_key_path
+  private_key_path = var.private_key_path
+  db_disk_image   = var.db_disk_image
+  subnet_id       = var.subnet_id
+  prov            = var.prov
 }
